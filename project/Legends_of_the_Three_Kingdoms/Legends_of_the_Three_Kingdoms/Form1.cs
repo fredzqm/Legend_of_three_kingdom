22d268603b0655cc392c48c519006aa6f37d13ff
wangsongyuf <wangs4@rose-hulman.edu>
2016-03-18 14:47:44 -0400

    create project

---
+++ project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Legends_of_the_Three_Kingdoms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Legends_of_the_Three_Kingdoms
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
