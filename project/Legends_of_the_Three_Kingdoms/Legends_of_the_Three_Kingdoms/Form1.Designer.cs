4583c732011331ff115fdaf3193739c0e327cbdb
wangsongyuf <wangs4@rose-hulman.edu>
2016-03-20 17:11:34 -0400

    add four players

--- project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Form1.Designer.cs
+++ project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace LOTK
+﻿using LOTK.View;
+
+namespace LOTK
 {
     partial class Form1
     {
@@ -6,6 +8,7 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private Required_Data data = new Required_Data();
 
         /// <summary>
         /// Clean up any resources being used.
@@ -28,20 +31,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+
+            this.Upperright = new System.Windows.Forms.RichTextBox();
+            this.DownLeft = new System.Windows.Forms.RichTextBox();
+            this.UpperLeft = new System.Windows.Forms.RichTextBox();
+            this.DownRight = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // Upperright
+            // 
+            this.Upperright.Location = new System.Drawing.Point(647, 30);
+            this.Upperright.Name = "Upperright";
+            this.Upperright.Size = new System.Drawing.Size(100, 96);
+            this.Upperright.TabIndex = 5;
+            this.Upperright.Text = data.upright_player+"\n"+data.upright_player_ab;
+            // 
+            // DownLeft
+            // 
+            this.DownLeft.Location = new System.Drawing.Point(52, 253);
+            this.DownLeft.Name = "DownLeft";
+            this.DownLeft.Size = new System.Drawing.Size(100, 96);
+            this.DownLeft.TabIndex = 6;
+            this.DownLeft.Text = data.downleft_player + "\n" + data.downleft_player_ab;
+            // 
+            // UpperLeft
+            // 
+            this.UpperLeft.Location = new System.Drawing.Point(52, 30);
+            this.UpperLeft.Name = "UpperLeft";
+            this.UpperLeft.Size = new System.Drawing.Size(100, 96);
+            this.UpperLeft.TabIndex = 7;
+            this.UpperLeft.Text = data.upleft_player+"\n"+ data.upleft_player_ab;
+            // 
+            // DownRight
+            // 
+            this.DownRight.Location = new System.Drawing.Point(647, 253);
+            this.DownRight.Name = "DownRight";
+            this.DownRight.Size = new System.Drawing.Size(100, 96);
+            this.DownRight.TabIndex = 8;
+            this.DownRight.Text = data.downright_player + "\n" + data.downright_player_ab; 
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 600);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.DownRight);
+            this.Controls.Add(this.UpperLeft);
+            this.Controls.Add(this.DownLeft);
+            this.Controls.Add(this.Upperright);
+            this.Name = data.this_player;
+            this.Text = data.this_player;
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox Upperright;
+        private System.Windows.Forms.RichTextBox DownLeft;
+        private System.Windows.Forms.RichTextBox UpperLeft;
+        private System.Windows.Forms.RichTextBox DownRight;
     }
 }
 
--- project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/View/Required_Data.cs
+++ project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/View/Required_Data.cs
@@ -4,16 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Legends_of_the_Three_Kingdoms.View
+namespace LOTK.View
 {
-    class Required_Data
+    public class Required_Data
     {   
         public string this_player;
         public string upleft_player;
+        public string upleft_player_ab;
         public string upright_player;
+        public string upright_player_ab;
         public string downleft_player;
+        public string downleft_player_ab;
         public string downright_player;
+        public string downright_player_ab;
         public string[] hold_cards;
         public string[] pool_cards;
+
+        public Required_Data()
+        {
+            this_player = "this_player";
+            upleft_player = "upleft_player";
+            upright_player = "upright_player";
+            downleft_player = "downleft_player";
+            downright_player = "downright_player";
+            upleft_player_ab = "upleft_player_ab";
+            upright_player_ab = "upright_player_ab";
+            downleft_player_ab = "downleft_player_ab";
+            downright_player_ab = "downright_player_ab";
+            hold_cards = new string[1] { "hold_cards" };
+            pool_cards = new string[1] { "pool_cards" };
+        }
     }
 }
