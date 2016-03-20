f64056b0ab410318730bbae666cd1dd2cd1dd5df
wangsongyuf <wangs4@rose-hulman.edu>
2016-03-20 18:36:12 -0400

    done with designing gui

--- project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Form1.Designer.cs
+++ project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Form1.Designer.cs
@@ -41,6 +41,8 @@ namespace LOTK
             this.tool_defence = new System.Windows.Forms.RichTextBox();
             this.hand_cards = new System.Windows.Forms.CheckedListBox();
             this.Pool = new System.Windows.Forms.RichTextBox();
+            this.ability = new System.Windows.Forms.Button();
+            this.done = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Upperright
@@ -49,7 +51,7 @@ namespace LOTK
             this.Upperright.Name = "Upperright";
             this.Upperright.Size = new System.Drawing.Size(100, 96);
             this.Upperright.TabIndex = 5;
-            this.Upperright.Text = data.upright_player+"\n"+data.upright_player_ab;
+            this.Upperright.Text = data.upright_player + "\n" + data.upright_player_ab;
             // 
             // DownLeft
             // 
@@ -57,7 +59,7 @@ namespace LOTK
             this.DownLeft.Name = "DownLeft";
             this.DownLeft.Size = new System.Drawing.Size(100, 96);
             this.DownLeft.TabIndex = 6;
-            this.DownLeft.Text = data.downleft_player+"\n"+data.downleft_player_ab;
+            this.DownLeft.Text = data.downleft_player + "\n" + data.downleft_player_ab;
             // 
             // UpperLeft
             // 
@@ -65,7 +67,7 @@ namespace LOTK
             this.UpperLeft.Name = "UpperLeft";
             this.UpperLeft.Size = new System.Drawing.Size(100, 96);
             this.UpperLeft.TabIndex = 7;
-            this.UpperLeft.Text = data.upleft_player+"\n"+data.upleft_player_ab;
+            this.UpperLeft.Text = data.upleft_player + "\n" + data.upleft_player_ab;
             // 
             // DownRight
             // 
@@ -73,7 +75,7 @@ namespace LOTK
             this.DownRight.Name = "DownRight";
             this.DownRight.Size = new System.Drawing.Size(100, 96);
             this.DownRight.TabIndex = 8;
-            this.DownRight.Text = data.downright_player+"\n"+data.downright_player_ab;
+            this.DownRight.Text = data.downright_player + "\n" + data.downright_player_ab;
             // 
             // turn
             // 
@@ -82,7 +84,7 @@ namespace LOTK
             this.turn.Name = "turn";
             this.turn.Size = new System.Drawing.Size(0, 17);
             this.turn.TabIndex = 9;
-            this.turn.Text = data.this_player+"\n"+data.this_player_stage;
+            this.turn.Text = data.this_player + "\n" + data.this_player_stage;
             // 
             // thisPlayer
             // 
@@ -90,7 +92,7 @@ namespace LOTK
             this.thisPlayer.Name = "thisPlayer";
             this.thisPlayer.Size = new System.Drawing.Size(100, 96);
             this.thisPlayer.TabIndex = 10;
-            this.thisPlayer.Text = data.this_player+"\n"+data.this_player_ab;
+            this.thisPlayer.Text = data.this_player + "\n" + data.this_player_ab;
             // 
             // tool_attack
             // 
@@ -98,7 +100,7 @@ namespace LOTK
             this.tool_attack.Name = "tool_attack";
             this.tool_attack.Size = new System.Drawing.Size(100, 96);
             this.tool_attack.TabIndex = 11;
-            this.tool_attack.Text = data.tool_attack+": "+data.tool_attack_ab;
+            this.tool_attack.Text = data.tool_attack +": "+ data.tool_attack_ab;
             // 
             // tool_defence
             // 
@@ -106,7 +108,7 @@ namespace LOTK
             this.tool_defence.Name = "tool_defence";
             this.tool_defence.Size = new System.Drawing.Size(100, 96);
             this.tool_defence.TabIndex = 12;
-            this.tool_defence.Text = data.tool_defence+"\n"+data.tool_defence_ab;
+            this.tool_defence.Text = data.tool_defence + ": " + data.tool_defence_ab;
             // 
             // hand_cards
             // 
@@ -120,7 +122,6 @@ namespace LOTK
 
                 this.hand_cards.Items.Insert(i, data.hold_cards[i]);
             }
-        
             // 
             // Pool
             // 
@@ -129,7 +130,7 @@ namespace LOTK
             this.Pool.Size = new System.Drawing.Size(320, 206);
             this.Pool.TabIndex = 14;
             string temp2 = "";
-            for(int i = 0; i < data.pool_cards.Length; i++)
+            for (int i = 0; i < data.pool_cards.Length; i++)
             {
                 if (i > 0) { temp2 = temp2 + "\n" + data.pool_cards[i]; }
                 else
@@ -137,11 +138,31 @@ namespace LOTK
             }
             this.Pool.Text = temp2;
             // 
+            // ability
+            // 
+            this.ability.Location = new System.Drawing.Point(243, 342);
+            this.ability.Name = "ability";
+            this.ability.Size = new System.Drawing.Size(75, 34);
+            this.ability.TabIndex = 15;
+            this.ability.Text = "Ability";
+            this.ability.UseVisualStyleBackColor = true;
+            // 
+            // done
+            // 
+            this.done.Location = new System.Drawing.Point(488, 511);
+            this.done.Name = "done";
+            this.done.Size = new System.Drawing.Size(75, 30);
+            this.done.TabIndex = 16;
+            this.done.Text = "Done";
+            this.done.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.done);
+            this.Controls.Add(this.ability);
             this.Controls.Add(this.Pool);
             this.Controls.Add(this.hand_cards);
             this.Controls.Add(this.tool_defence);
@@ -170,6 +191,8 @@ namespace LOTK
         private System.Windows.Forms.RichTextBox tool_defence;
         private System.Windows.Forms.CheckedListBox hand_cards;
         private System.Windows.Forms.RichTextBox Pool;
+        private System.Windows.Forms.Button ability;
+        private System.Windows.Forms.Button done;
     }
 }
 
