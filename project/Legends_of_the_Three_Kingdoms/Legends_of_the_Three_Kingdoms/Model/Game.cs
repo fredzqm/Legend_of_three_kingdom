62603ad92e5be9029ecfe6f2c2daa25efde00a7f
Fred Zhang <zhangq2@rose-hulman.edu>
2016-03-20 00:16:36 -0400

    GameConstructTest pass

--- project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Model/Game.cs
+++ project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Model/Game.cs
@@ -2,5 +2,13 @@
 {
     public class Game
     {
+        private readonly int Num_Player;
+        public StageList stages { get; set; }
+        public Game(int Num_player)
+        {
+            Num_Player = Num_player;
+            stages = new StageList();
+            stages.add(new PlayerTurn(0));
+        }
     }
 }
