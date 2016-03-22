8eb13950e460b86742efdc3911d7e7caedb565cf
Fred Zhang <fredzqm@gmail.com>
2016-03-22 18:37:28 -0400

    Change the interface for Card and CardSet, cannot add more cards to an card set, all cards must be specified in the contructor with an ICollection. Also, updates many tests

--- project/Legends_of_the_Three_Kingdoms/LOTK_Test/ModelTest/CardTest.cs
+++ project/Legends_of_the_Three_Kingdoms/LOTK_Test/ModelTest/CardTest.cs
@@ -12,27 +12,29 @@ namespace LOTK_Test.ModelTest
         [TestMethod]
         public void CardSetConstructTest() {
             Card c = new Card(CardSuit.Club, CardType.Attack, 0);
-            CardSet s = new CardSet(10);
-            s[0] = c;
-            Assert.AreSame(c, s[0]);
+            ICollection<Card> ls = new List<Card>();
+            ls.Add(c);
+            CardSet s = new CardSet(ls);
+            Assert.AreEqual(c, s[0]);
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(Exception))]
-        public void RedefineCardTest()
-        {
-            Card c = new Card(CardSuit.Club, CardType.Attack, 0);
-            Card b = new Card(CardSuit.Club, CardType.Miss, 1);
-            CardSet s = new CardSet(10);
-            s[0] = c;
-            s[0] = b;
-        }
+        //[TestMethod]
+        //[ExpectedException(typeof(Exception))]
+        //public void RedefineCardTest()
+        //{
+        //    Card c = new Card(CardSuit.Club, CardType.Attack, 0);
+        //    Card b = new Card(CardSuit.Club, CardType.Miss, 1);
+        //    CardSet s = new CardSet(10);
+        //    s.addCard();
+        //    s[0] = c;
+        //    s[0] = b;
+        //}
 
         [TestMethod]
         [ExpectedException(typeof(Exception))]
         public void DiscardUndefinedCardExceptionTest()
         {
-            List<Card> ls = new List<Card>();
+            ICollection<Card> ls = new List<Card>();
             ls.Add(new Card(CardSuit.Club, CardType.Attack, 0));
             ls.Add(new Card(CardSuit.Club, CardType.Miss, 1));
             ls.Add(new Card(CardSuit.Diamond, CardType.Miss, 2));
@@ -46,8 +48,9 @@ namespace LOTK_Test.ModelTest
         public void ShuffleOneCardTest()
         {
             Card a = new Card(CardSuit.Club, CardType.Attack, 0);
-            CardSet s = new CardSet(1);
-            s[0] = a;
+            ICollection<Card> ls = new List<Card>();
+            ls.Add(a);
+            CardSet s = new CardSet(ls);
             s.shuffle();
             Assert.AreEqual(a, s.pop());
         }
@@ -57,9 +60,10 @@ namespace LOTK_Test.ModelTest
         {
             Card a = new Card(CardSuit.Club, CardType.Attack, 0);
             Card b = new Card(CardSuit.Club, CardType.Miss, 1);
-            CardSet s = new CardSet(2);
-            s[0] = a;
-            s[1] = b;
+            ICollection<Card> ls = new List<Card>();
+            ls.Add(a);
+            ls.Add(b);
+            CardSet s = new CardSet(ls);
             s.shuffle();
             Card x = s.pop();
             Card y = s.pop();
@@ -144,33 +148,33 @@ namespace LOTK_Test.ModelTest
             }
         }
 
-        [TestMethod]
-        public void CardPileNotFullTest()
-        {
-            List<Card> ls = new List<Card>();
-            ls.Add(new Card(CardSuit.Club, CardType.Attack, 0));
-            ls.Add(new Card(CardSuit.Club, CardType.Miss, 1));
-            ls.Add(new Card(CardSuit.Diamond, CardType.Miss, 2));
-            ls.Add(new Card(CardSuit.Spade, CardType.Attack, 3));
-            ls.Add(new Card(CardSuit.Club, CardType.Wine, 4));
-            List<Card> ls2 = new List<Card>(ls);
+        //[TestMethod]
+        //public void CardPileRepeatTest()
+        //{
+        //    List<Card> ls = new List<Card>();
+        //    ls.Add(new Card(CardSuit.Club, CardType.Attack, 0));
+        //    ls.Add(new Card(CardSuit.Club, CardType.Miss, 1));
+        //    ls.Add(new Card(CardSuit.Diamond, CardType.Miss, 2));
+        //    ls.Add(new Card(CardSuit.Spade, CardType.Attack, 3));
+        //    ls.Add(new Card(CardSuit.Club, CardType.Wine, 4));
+        //    List<Card> ls2 = new List<Card>(ls);
 
-            int size = ls.Count;
-            CardSet s = new CardSet(size + 10);
-            for (int i = 0; i < size; i++)
-            {
-                s[i] = ls[i];
-            }
-            s.shuffle();
+        //    int size = ls.Count;
+        //    CardSet s = new CardSet(size + 10);
+        //    for (int i = 0; i < size; i++)
+        //    {
+        //        s[i] = ls[i];
+        //    }
+        //    s.shuffle();
 
-            for (int i = 0; i < size; i++)
-            {
-                Card c = s.pop();
-                Assert.IsTrue(ls.Contains(c));
-                ls.Remove(c);
-            }
-            Assert.AreEqual(0, ls.Count);
-        }
+        //    for (int i = 0; i < size; i++)
+        //    {
+        //        Card c = s.pop();
+        //        Assert.IsTrue(ls.Contains(c));
+        //        ls.Remove(c);
+        //    }
+        //    Assert.AreEqual(0, ls.Count);
+        //}
 
     }
 }
--- project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Model/Card.cs
+++ project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Model/Card.cs
@@ -6,54 +6,6 @@ using System.Threading.Tasks;
 
 namespace LOTK.Model
 {
-    public class Card
-    {
-        private CardType type;
-        private CardSuit suit;
-        private byte num;
-
-        private int id = -1;
-        public int CardID { get { return id; } }
-
-        internal void setCardId(int value)
-        {
-            if (id == -2)
-                throw new Exception("CardID cannot be redifined");
-            id = value;
-        }
-
-        public Card(CardSuit s, CardType t, byte n)
-        {
-            this.suit = s;
-            this.type = t;
-            this.num = n;
-        }
-
-        public override bool Equals(object obj)
-        {
-            Card x = obj as Card;
-            return (x != null) && (type == x.type) && (suit == x.suit) && (num == x.num);
-        }
-        public override int GetHashCode()
-        {
-            return type.GetHashCode() + suit.GetHashCode() + num.GetHashCode();
-        }
-        public override string ToString()
-        {
-            return String.Format("Card {0}  {1}{3}", type, suit, num);
-        }
-
-        internal string getDescription()
-        {
-            throw new NotImplementedException();
-        }
-
-        internal string getName()
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     public class CardSet
     {
         private Card[] ls;
@@ -129,6 +81,56 @@ namespace LOTK.Model
         }
 
     }
+
+    public class Card
+    {
+        private CardType type;
+        private CardSuit suit;
+        private byte num;
+
+        private int id = -1;
+        public int CardID { get { return id; } }
+
+        internal void setCardId(int value)
+        {
+            if (id == -1)
+                throw new Exception("CardID cannot be redifined");
+            id = value;
+        }
+
+        public Card(CardSuit s, CardType t, byte n)
+        {
+            this.suit = s;
+            this.type = t;
+            this.num = n;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Card x = obj as Card;
+            return (x != null) && (type == x.type) && (suit == x.suit) && (num == x.num);
+        }
+        public override int GetHashCode()
+        {
+            return type.GetHashCode() + suit.GetHashCode() + num.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return String.Format("Card {0}  {1}{3}", type, suit, num);
+        }
+
+        internal string getDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string getName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    
 
 
     public enum CardSuit
