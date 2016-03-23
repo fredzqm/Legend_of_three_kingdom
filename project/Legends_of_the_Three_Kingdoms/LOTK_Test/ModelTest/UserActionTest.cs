2157e84b4af6f371bdb19338ce5a65f4f16774c9
dingy2@rose-hulman.edu <dingy2@rose-hulman,edu>
2016-03-23 19:36:39 -0400

    Add exceptionhandler

---
+++ project/Legends_of_the_Three_Kingdoms/LOTK_Test/ModelTest/UserActionTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOTK_Test.ModelTest
+{
+    class Class1
+    {
+    }
+}
---
+++ project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Model/ExceptionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legends_of_the_Three_Kingdoms.Model
+{
+    public class NotDefineException: Exception
+    {
+        public NotDefineException(String message)
+            : base(message) { 
+        }
+    }
+
+    public class EmptyException: Exception
+    {
+        public EmptyException(String message)
+             : base(message) {
+        }
+    }
+
+    public class NoCardException : Exception
+    {
+        public NoCardException(String message)
+             : base(message)
+        {
+        }
+    }
+
+}
