cb32ffea8a4b016a8dd00cf522764ef515328a54
dingy2@rose-hulman.edu <dingy2@rose-hulman,edu>
2016-03-23 20:00:42 -0400

    EmptyPhaseListtest failed

--- project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Model/Phase.cs
+++ project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Model/Phase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Legends_of_the_Three_Kingdoms.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -83,10 +84,10 @@ namespace LOTK.Model
 
         public Phase pop()
         {
-            if (isEmpty())
+/*            if (isEmpty())
             {
-                throw new Exception("Empty PhaseList Exception");
-            }
+                throw new EmptyException("Empty PhaseList Exception");
+            } */
             Phase retStage = head.stage;
             if (head == tail)
             { // empty
