f41abc601362184d45597e131ad33f1b80b8317c
dingy2@rose-hulman.edu <dingy2@rose-hulman,edu>
2016-03-23 20:49:31 -0400

    Testpushstage emptylist passed

--- project/Legends_of_the_Three_Kingdoms/LOTK_Test/ModelTest/PhaseListTest.cs
+++ project/Legends_of_the_Three_Kingdoms/LOTK_Test/ModelTest/PhaseListTest.cs
@@ -39,21 +39,37 @@ namespace LOTK_Test.ModelTest
         }
 
         [TestMethod]
+        public void TestPushOneStageList()
+        {
+            PhaseList ls = new PhaseList();
+            ls.add(new Phase(new Player(0), PhaseType.PlayerTurn));
+            Assert.AreEqual(ls.pop().playerID, 0);
+            try
+            {
+                ls.pop();
+            }
+            catch (EmptyException e)
+            {
+                Console.WriteLine("Empty Exception caught.", e);
+            }
+        }
+
+        [TestMethod]
         public void TestPushStageList()
         {
             PhaseList ls = new PhaseList();
             ls.add(new Phase(new Player(0), PhaseType.PlayerTurn));
-/*            ls.add(new Phase(new Player(1), PhaseType.PlayerTurn));
+            ls.add(new Phase(new Player(1), PhaseType.PlayerTurn));
             ls.add(new Phase(new Player(2), PhaseType.PlayerTurn));
             PhaseList ls2 = new PhaseList();
             ls2.add(new Phase(new Player(3), PhaseType.PlayerTurn));
             ls2.add(new Phase(new Player(4), PhaseType.PlayerTurn));
             ls.pushStageList(ls2); 
             Assert.AreEqual(ls.pop().playerID, 3);
-            Assert.AreEqual(ls.pop().playerID, 4); */
+            Assert.AreEqual(ls.pop().playerID, 4); 
             Assert.AreEqual(ls.pop().playerID, 0);
-/*            Assert.AreEqual(ls.pop().playerID, 1);
-            Assert.AreEqual(ls.pop().playerID, 2); */
+            Assert.AreEqual(ls.pop().playerID, 1);
+            Assert.AreEqual(ls.pop().playerID, 2); 
         }
     }
 
