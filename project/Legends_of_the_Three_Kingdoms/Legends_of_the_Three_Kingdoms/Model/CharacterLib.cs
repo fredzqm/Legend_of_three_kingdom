fb4ab1a1e5206547041cda345e99b2d23ffbef41
wangsongyuf <wangs4@rose-hulman.edu>
2016-04-10 23:06:45 -0400

    TDD CaoCaoAbtest pass

--- project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Model/CharacterLib.cs
+++ project/Legends_of_the_Three_Kingdoms/Legends_of_the_Three_Kingdoms/Model/CharacterLib.cs
@@ -50,16 +50,17 @@ namespace LOTK.Model
         public CaoCao(int pos) : base(pos, "Cao Cao", "When Cao Cao is damaged by a card, he can immediately put it into his hand", 4)
         {
         }
-        //    public override  PhaseList harm(HarmPhase harmPhase, IGame game) { 
-        
-        //    this.health -= harmPhase.harm;
-        //    this.handCards.Add(harmPhase.card);
-        //    if (health < 0)
-        //    {
-        //        return new PhaseList(new AskForHelpPhase(this));
-        //    }
-        //    return new PhaseList();
-        //}
+        public override PhaseList harm(HarmPhase harmPhase, IGame game)
+        {
+
+            this.health -= harmPhase.harm;
+            this.handCards.Add(harmPhase.card);
+            if (health < 0)
+            {
+                return new PhaseList(new AskForHelpPhase(this));
+            }
+            return new PhaseList();
+        }
     }
 
 
