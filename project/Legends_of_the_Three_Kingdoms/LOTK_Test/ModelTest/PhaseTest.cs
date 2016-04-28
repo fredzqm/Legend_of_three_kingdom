1eefcf0867580ca095e16c9b8862f9baded02300
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-04-28 19:16:58 -0400

    DyingAskForHelpDeadTest1 fails

--- project/Legends_of_the_Three_Kingdoms/LOTK_Test/ModelTest/PhaseTest.cs
+++ project/Legends_of_the_Three_Kingdoms/LOTK_Test/ModelTest/PhaseTest.cs
@@ -829,5 +829,101 @@ namespace LOTK_Test.ModelTest
             Assert.IsTrue(ls.isEmpty());
             // ActionPhase produces attackPhase
         }
+
+        [TestMethod]
+        public void DyingAskForHelpDeadTest1()
+        {
+            Player dying = new Player(0, "dying", "dying", 1);
+            Player source = mocks.Stub<Player>(1);
+            Player player2 = mocks.Stub<Player>(2);
+            Player player3 = mocks.Stub<Player>(3);
+            Player[] players = new Player[4];
+            Phase p1, p2, p3, p4, p5;
+            PhaseList ls;
+            players[0] = dying;
+            players[1] = source;
+            players[2] = player2;
+            players[3] = player3;
+
+            Attack card = new Attack(CardSuit.Club, 1);
+            Peach peach = new Peach(CardSuit.Heart, 2);
+            Assert.AreEqual(1, dying.healthLimit);
+            ICardSet cardStack = MockRepository.GenerateStub<ICardSet>();
+
+            IGame game = MockRepository.GenerateStub<IGame>();
+            game.Stub(x => x.players).Return(players);
+            game.Stub(x => x.nextPlayer(source, 0)).Return(source);
+            game.Stub(x => x.nextPlayer(source, 1)).Return(player2);
+            game.Stub(x => x.nextPlayer(source, 2)).Return(player3);
+            game.Stub(x => x.nextPlayer(source, 3)).Return(dying);
+            game.Stub(x => x.curRoundPlayer).Return(source);
+            game.Stub(s => s.cards).Return(cardStack);
+
+            HarmPhase harm = new HarmPhase(dying, source, 1, card);
+            ls = harm.advance(null, game);
+            p1 = ls.pop();
+            Assert.IsInstanceOfType(p1, typeof(AskForHelpPhase));
+            Assert.IsTrue(ls.isEmpty());
+
+            ls = p1.advance(null, game);
+            p2 = ls.pop();
+            p3 = ls.pop();
+            Assert.IsTrue(ls.isEmpty());
+            Assert.IsInstanceOfType(p2, typeof(ResponsePhase));
+            Assert.AreEqual(source, p2.player);
+            Assert.AreSame(p1, p3);
+
+            ls = p2.advance(new YesOrNoAction(false), game);
+            Assert.IsTrue(ls.isEmpty());
+
+            ls = p3.advance(null, game);
+            p4 = ls.pop();
+            p5 = ls.pop();
+            Assert.IsTrue(ls.isEmpty());
+            Assert.IsInstanceOfType(p4, typeof(ResponsePhase));
+            Assert.AreEqual(player2, p4.player);
+            Assert.AreSame(p1, p5);
+
+            ls = p4.advance(new YesOrNoAction(false), game);
+            Assert.IsTrue(ls.isEmpty());
+
+            ls = p5.advance(null, game);
+            p4 = ls.pop();
+            p5 = ls.pop();
+            Assert.IsTrue(ls.isEmpty());
+            Assert.IsInstanceOfType(p4, typeof(ResponsePhase));
+            Assert.AreEqual(player3, p4.player);
+            Assert.AreSame(p1, p5);
+
+            ls = p4.advance(new YesOrNoAction(false), game);
+            Assert.IsTrue(ls.isEmpty());
+
+            ls = p5.advance(null, game);
+            p4 = ls.pop();
+            p5 = ls.pop();
+            Assert.IsTrue(ls.isEmpty());
+            Assert.IsInstanceOfType(p4, typeof(ResponsePhase));
+            Assert.AreEqual(dying, p4.player);
+            Assert.AreSame(p1, p5);
+
+            ls = p4.advance(new YesOrNoAction(false), game);
+            Assert.IsTrue(ls.isEmpty());
+
+            ls = p5.advance(null, game);
+            p4 = ls.pop();
+            p5 = ls.pop();
+            Assert.IsTrue(ls.isEmpty());
+            Assert.IsInstanceOfType(p4, typeof(DeadPhase));
+            Assert.AreEqual(dying, p4.player);
+            Assert.AreSame(p1, p5);
+
+            ls = p4.advance(null, game);
+            Assert.IsTrue(ls.isEmpty());
+            Assert.IsTrue(dying.isDead());
+
+            ls = p5.advance(null, game);
+            Assert.IsTrue(ls.isEmpty());
+            // ActionPhase produces attackPhase
+        }
     }
 }
