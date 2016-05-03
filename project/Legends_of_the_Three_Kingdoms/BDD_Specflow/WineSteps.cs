2fdd4fb9eab3c63f28b3ff350994f42a385412de
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-05-02 22:14:48 -0400

    wine attack test fails

--- project/Legends_of_the_Three_Kingdoms/BDD_Specflow/WineSteps.cs
+++ project/Legends_of_the_Three_Kingdoms/BDD_Specflow/WineSteps.cs
@@ -1,45 +1,66 @@
-﻿using System;
+﻿using LOTK.Controller;
+using LOTK.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using TechTalk.SpecFlow;
 
-namespace BDD_Specflow
+namespace LOTK_Test.BDD
 {
     [Binding]
     public class WineSteps
     {
-        [Given(@"There is a game of player A with (.*) health and player B has (.*) health")]
+        public Player playerA, playerB;
+        public IGame game;
+        [Given(@"There is a game of player A with (.) health and player B has (.) health")]
         public void GivenThereIsAGameOfPlayerAWithHealthAndPlayerBHasHealth(int p0, int p1)
         {
-            ScenarioContext.Current.Pending();
+            playerA = new Player(0, "PA", "Player A", p0);
+            playerB = new Player(1, "PB", "Player B", p1);
+            Player[] players = new Player[2];
+            players[0] = playerA;
+            players[1] = playerB;
+            game = new Game(players, GameController.initialLizeCardSet());
+            game.start();
         }
-        
+
+        public ActionPhase actionPhase;
         [Given(@"At Player A's actionPhase")]
         public void GivenAtPlayerASActionPhase()
         {
-            ScenarioContext.Current.Pending();
+            actionPhase = game.curPhase as ActionPhase;
+            while (actionPhase == null || actionPhase.player != playerA)
+            {
+                game.tick();
+                actionPhase = game.curPhase as ActionPhase;
+            }
         }
-        
+
+        public Wine wine;
         [Given(@"Player A uses Wine")]
         public void GivenPlayerAUsesWine()
         {
-            ScenarioContext.Current.Pending();
+            wine = new Wine(CardSuit.Club, 1);
+            game.processUserInput(0, new UseCardAction(wine));
         }
-        
+
+        public Attack attack;
         [Given(@"Player A attack Player B")]
         public void GivenPlayerAAttackPlayerB()
         {
-            ScenarioContext.Current.Pending();
+            attack = new Attack(CardSuit.Club, 2);
+            game.processUserInput(0, new UseCardAction(attack, playerB));
         }
-        
+
         [When(@"Player B response with cancel")]
         public void WhenPlayerBResponseWithCancel()
         {
-            ScenarioContext.Current.Pending();
+            game.processUserInput(1, new YesOrNoAction(false));
         }
-        
-        [Then(@"Player B should has (.*) health")]
+
+        [Then(@"Player B should has (.) health")]
         public void ThenPlayerBShouldHasHealth(int p0)
         {
-            ScenarioContext.Current.Pending();
+            Assert.AreEqual(p0, playerB.health);
         }
     }
 }
