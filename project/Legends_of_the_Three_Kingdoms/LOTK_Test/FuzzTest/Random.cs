965363280cc161d4bdb5405b4d1f04143b2c4ed0
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-05-10 17:07:57 -0400

    General randomTester frame ready

---
+++ project/Legends_of_the_Three_Kingdoms/LOTK_Test/FuzzTest/Random.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LOTK.Controller;
+
+namespace LOTK_Test.FuzzTest { 
+
+    [TestClass]
+    public class RandomTester
+    {
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            GameController game = new GameController();
+            Randomizor tester = new Randomizor(game.game);
+            tester.start(100);
+        }
+    }
+}
---
+++ project/Legends_of_the_Three_Kingdoms/LOTK_Test/FuzzTest/Randomizor.cs
@@ -0,0 +1,63 @@
+﻿using LOTK.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOTK_Test.FuzzTest
+{
+    class Randomizor
+    {
+        private IGame game;
+        private Random random;
+
+        public Randomizor(IGame game)
+        {
+            this.game = game;
+            this.random = new Random();
+        }
+
+        public void start(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                randomInputResponse();
+            }  
+        }
+
+        private void randomInputResponse()
+        {
+            UserAction action;
+            int r = random.Next();
+            switch(r % 10)
+            {
+                case 1:
+                    action = new CardAction(randomCard());
+                    break;
+                case 2:
+                    if (random.Next() % 2 == 0) {
+                        action = new YesOrNoAction(true);
+                    } else
+                    {
+                        action = new YesOrNoAction(false);
+                    }
+                    break;
+                default:
+                    action = new UseCardAction(randomCard(), randomPlayer());
+                    break;
+            }
+            game.processUserInput(game.curPhase.playerID, action);
+        }
+
+        private Player randomPlayer()
+        {
+            return game.players[random.Next() % game.Num_Player];
+        }
+
+        private Card randomCard()
+        {
+            return game.cards[random.Next() % game.cards.cardPileCount];
+        }
+    }
+}
